9d3ced995c9f758918bb5006999e68d05b3de98b
Leor Manelis <leormanelis@gmail.com>
2013-08-05 22:22:22 -0600

    created a simple form to add students to a database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = 'df80170d5f380beab39d0e4ec446d5d64bb1ea6b0d6cfc9c9d903d39dd3de25c842c5d15bdc0b0182a31747d4567943061545c3bf12db37e3172b41f4dcca9f3'
